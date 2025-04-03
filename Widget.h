77fff02ac30d43ad1cc5d8249a9f9828a86abe0e
bastien-muraccioli <bastien.muraccioli.pro@gmail.com>
2025-04-03 14:15:08 +0900

    Ubuntu24 fix

--- Widget.h
+++ Widget.h
@@ -7,11 +7,37 @@
 #include <memory>
 #include <string>
 
-#ifdef SPDLOG_FMT_EXTERNAL
-#  include <fmt/ranges.h>
-#else
-#  include <spdlog/fmt/bundled/ranges.h>
-#endif
+// #ifdef SPDLOG_FMT_EXTERNAL
+// #  include <fmt/ranges.h>
+// #else
+// #  include <spdlog/fmt/bundled/ranges.h>
+// #endif
+
+// #include <mc_rtc/fmt_eigen.h>
+// #include <fmt/core.h>
+// #include <vector>
+
+#include <fmt/core.h>
+#include <fmt/ranges.h>
+#include <string_view>
+#include <vector>
+#include <string>
+#include <Eigen/Dense>
+
+template <> 
+struct fmt::formatter<Eigen::VectorXd> : fmt::formatter<std::string> {
+    template <typename FormatContext>
+    auto format(const Eigen::VectorXd& vec, FormatContext& ctx) {
+        std::string result = "[";
+        for (int i = 0; i < vec.size(); ++i) {
+            result += fmt::format("{}{}", vec[i], (i < vec.size() - 1 ? ", " : ""));
+        }
+        result += "]";
+        return formatter<std::string>::format(result, ctx);
+    }
+};
+
+
 
 namespace mc_rtc::imgui
 {
